be2d9994932de01abc27430de6125939949656d3
candiceywu <candice.ying.wu@gmail.com>
2021-10-12 16:59:25 -0700

    drafted footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <section className="footer">
+            <h1>let's connect IRL.</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com/in/candicewu/" target="_blank">linkedin.</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/candiceywu" target="_blank">github.</a>
+                    </li>
+                    <li>
+                        <a href="mailto: candice.ying.wu@gmail.com" target="_blank">email.</a>
+                    </li>
+                    <li>
+                        <a href="../assets/resume/Candice Wu resume.pdf" target="_blank">resume.</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+    )
+}
+
+export default Footer;
