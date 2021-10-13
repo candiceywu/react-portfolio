4acba7506569bb00d8554c6a23c24e5e4cacb7c4
candiceywu <candice.ying.wu@gmail.com>
2021-10-12 17:09:45 -0700

    drafting header.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+// import Navigation from './components/Navigation';
 import Portfolio from './components/Portfolio';
+// import Project from './components/Project';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
