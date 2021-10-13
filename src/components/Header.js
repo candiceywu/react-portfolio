8d05a6fdb37a639ec5691871664dcbc26f9812d7
candiceywu <candice.ying.wu@gmail.com>
2021-10-12 17:53:35 -0700

    added Components for linking in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 import About from '../components/Navigation';
 import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 
+//format for creating a link between pages in React Router (using Component)
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <header>
+                    <Navigation />
+                </header>
 
-function Header() {
-    return (
-        <header className="header">
-        <h1>hi there, i'm candice.</h1>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#about">about.</a>
-                </li>
-                <li>
-                    <a href="#work">work.</a>
-                </li>
-                <li>
-                    <a href="#contact">contact.</a>
-                </li>
-            </ul>
-        </nav>
-    </header>  
-    )
+                <section className="header">
+                    <h1>hi there, i'm candice.</h1>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Route exact path="/about" component={About} />
+                            </li>
+                            <li>
+                                <Route exact path="/portfolio" component={Portfolio} />
+                            </li>
+                            <li>
+                                <Route exact path="/contact" component={Contact} />
+                            </li>
+                        </ul>
+                    </nav>
+                </section>
+            </HashRouter>
+        )
+    }
 }
 
 export default Header;
